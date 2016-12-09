56fb43cc47f67d66f0c4536f109018c0e35fd1d5
Andrei.Tsitou <andrei_tsitou@epam.com>
2016-12-09 11:02:54 +0300

    temp weather style fixing

--- app/src/components/weather/weather.tpl.ts
+++ app/src/components/weather/weather.tpl.ts
@@ -1,28 +1,9 @@
 export const template: string = `
 <div>
     <div>Weather in towns: </div>
-                
-        
-    <!--<div class="townstable">$ {this.townTableRender}</div>-->
-    
-    <!-- <div class="rowelement">        
-        <table class="tablerow">
-            <tr>
-                <td><span class="townname">$ {rowObject.name}</span></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td><span>Temperature: </span><span>$ {Math.round(rowObject.main.temp-273.15)}</span></td>
-                <td><span>Humidity: </span><span>$ {rowObject .main.humidity||''}</span></td>
-                <td><span>Wind: </span><span>$ {rowObject.wind.speed||''}</span></td>
-            </tr>
-        </table>        
-    </div>
-    -->
-    
+      
     <ul>
-        <li *ngFor="let town of townsTable">
+        <li *ngFor="let town of townsTable" class="rowelement">
             <table class="tablerow">
               <tr>
                   <td><span class="townname">{{town.name}}</span></td>
@@ -39,4 +20,4 @@ export const template: string = `
     </ul>    
     
 </div>
-  `;
+`;
