91f49094ffe5d211c719ec050725a1b4494b330d
Andrei.Tsitou <andrei_tsitou@epam.com>
2016-12-09 13:09:39 +0300

    add ngZone

--- app/src/components/weather/weather.component.ts
+++ app/src/components/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output } from '@angular/core';
+import {Component, Input, Output, NgZone} from '@angular/core';
 import {template} from './weather.tpl';
 
 import {Weather} from './weather.d';
@@ -12,7 +12,7 @@ import {LocationService} from '../common/location.service'
 })
 export class WeatherComponent {
   @Input() amounttowns: string;
-  @Output() updateMarkers: Function;
+  // @Output() updateMarkers: Function;
 
   API: string = `94c7919f6854ca11558382472a998f8f`;
   // cnt: string = '10';
@@ -24,103 +24,44 @@ export class WeatherComponent {
   // townTableTemp: string = '';
   // townTableRender: string = 'Loading';
 
+  trigLoad: boolean = false;
   townsTable: Weather.ITownWeather[] ;
 
-  callbackDownloadFunction: Function;
+  // callbackDownloadFunction: Function;
 
-  constructor(/*restService: RestService*/){
+  constructor(
+    private zone: NgZone){
+
     console.log("WeatherComponent");
+    this.townsTable = [];
     LocationService.getCurrentLocation().then(
       (coordinate: Coordinates) => {
         this.downloadWeatherInCircle(coordinate.latitude, coordinate.longitude, parseInt(this.amounttowns));
       }
     )
-
   }
 
-  // callbackLocation(coordinate: Coordinates) {
-  //   this.downloadWeatherInCircle(coordinate.latitude, coordinate.longitude, 1, this.callbackDownloadFunction);
-  // }
-
-  // private _initTable(){
-  //   return `<div class='tablewrapper'>`
-  // }
-  // _addLineToTable(tableQuery: string, row: string){
-  //   tableQuery.concat(row);
-  // }
-  // private _endTable(){
-  //   return '</div>';
-  // }
-  // private generateTableRow(rowObject: Weather.ITownWeather){
-  //   return `<div class="rowelement">
-  //       <table class="tablerow">
-  //           <tr>
-  //               <td><span class="townname">${rowObject.name}</span></td>
-  //               <td></td>
-  //               <td></td>
-  //           </tr>
-  //           <tr>
-  //               <td><span>Temperature: </span><span>${Math.round(rowObject.main.temp-273.15)}</span></td>
-  //               <td><span>Humidity: </span><span>${rowObject.main.humidity||''}</span></td>
-  //               <td><span>Wind: </span><span>${rowObject.wind.speed||''}</span></td>
-  //           </tr>
-  //       </table>
-  //     </div>`
-  // }
-
-  // private generateTownTable(array: Weather.ITownWeather[], context: WeatherComponent){
-  //   // context.townTableTemp = context._initTable();
-  //   // array.forEach((value, index, array) => {
-  //   //   context.townTableTemp = context.townTableTemp.concat(context.generateTableRow(value));
-  //   // });
-  //   // context.townTableTemp = context.townTableTemp.concat(context._endTable());
-  //
-  //   // context.townTableRender = String(context.townTableTemp);
-  //   // context.updateTowsTable(context);
-  // }
-
-  // private updateTowsTable(context: WeatherComponent){
-  //   document.querySelector('.townstable').innerHTML = context.townTableRender;
-  // }
-
-
-  // private callBackResponseList(data: string, context: WeatherComponent){
-  //   if (data !== null){
-  //     context.weatherObject = <Weather.IWeatherResponse> JSON.parse(data);
-  //
-  //     context.townsTable = context.weatherObject.list;
-  //
-  //     // context.generateTownTable(context.weatherObject.list, context);
-  //     // context.updateTowsTable(context);
-  //     if (context.callbackDownloadFunction !== undefined){
-  //       context.callbackDownloadFunction();
-  //     }
-  //   } else {
-  //     console.log('Cann\'t load data from weather portal!');
-  //     alert('Cann\'t load data from weather portal!');
-  //   }
-  // }
+  private updateTableList(responseText: string){
+    if (responseText !== null){
+      this.weatherObject = <Weather.IWeatherResponse> JSON.parse(responseText);
+      this.townsTable = this.weatherObject.list;
+      // if (this.callbackDownloadFunction !== undefined){
+      //   this.callbackDownloadFunction();
+      // }
+      console.log(this);
+      this.trigLoad = true;
+      this.zone.run(() => {});
+    } else {
+      console.log('Cann\'t load data from weather portal!');
+      alert('Cann\'t load data from weather portal!');
+    }
+  }
 
   downloadWeatherInCircle(latitude: number, longitude: number, count: number){
     let urlTemplate: string = `http://api.openweathermap.org/data/2.5/find?lat=${latitude}&lon=${longitude}&cnt=${count}&appid=${this.API}`;
-    // this.callbackDownloadFunction = callbackFunction;
-    // RestService.sendRequest(this.typeRequest, urlTemplate, this.async, this.callBackResponseList, this, '');
     RestService.sendRequest(this.typeRequest, urlTemplate, this.async, '').then(
       (responseText: string) => {
-        if (responseText !== null){
-          this.weatherObject = <Weather.IWeatherResponse> JSON.parse(responseText);
-
-          this.townsTable = this.weatherObject.list;
-
-          // context.generateTownTable(context.weatherObject.list, context);
-          // context.updateTowsTable(context);
-          if (this.callbackDownloadFunction !== undefined){
-            this.callbackDownloadFunction();
-          }
-        } else {
-          console.log('Cann\'t load data from weather portal!');
-          alert('Cann\'t load data from weather portal!');
-        }
+        this.updateTableList(responseText);
       }
     );
   }
--- app/src/components/weather/weather.tpl.ts
+++ app/src/components/weather/weather.tpl.ts
@@ -2,22 +2,24 @@ export const template: string = `
 <div>
     <div>Weather in towns: </div>
       
-    <ul>
-        <li *ngFor="let town of townsTable" class="rowelement">
-            <table class="tablerow">
-              <tr>
-                  <td><span class="townname">{{town.name}}</span></td>
-                  <td></td>
-                  <td></td>
-              </tr>
-              <tr>
-                  <td><span>Temperature: </span><span>{{town.main.temp-273.15}}</span></td>
-                  <td><span>Humidity: </span><span>{{town.main.humidity||''}}</span></td>
-                  <td><span>Wind: </span><span>{{town.wind.speed||''}}</span></td>
-              </tr>
-          </table>
-        </li>
-    </ul>    
+    <div *ngIf="trigLoad">
+        <ul>
+          <li *ngFor="let town of townsTable" class="rowelement">
+              <table class="tablerow">
+                <tr>
+                    <td><span class="townname">{{town.name}}</span></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><span>Temperature: </span><span>{{town.main.temp-273.15}}</span></td>
+                    <td><span>Humidity: </span><span>{{town.main.humidity||''}}</span></td>
+                    <td><span>Wind: </span><span>{{town.wind.speed||''}}</span></td>
+                </tr>
+            </table>
+          </li>
+      </ul>
+    </div>
     
 </div>
 `;
